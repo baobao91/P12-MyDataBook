6d3189432e43bab50b1d1f07477f455c049417f7
15017185 <15017185@myrp.edu.sg>
2017-08-11 15:43:01 +0800

    2

--- app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/AboutActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/AboutActivity.java
@@ -1,14 +1,20 @@
 package sg.edu.rp.webservices.p12_mydatabook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
 
     }
