5a4f3550d082d64b86ea46f4f2ac6a5da36247c8
15017185 <15017185@myrp.edu.sg>
2017-08-11 14:50:33 +0800

    123

--- app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/MainActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/p12_mydatabook/MainActivity.java
@@ -1,6 +1,7 @@
 package sg.edu.rp.webservices.p12_mydatabook;
 
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -8,17 +9,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+//123
     private DrawerLayout drawerLayout;
     private ListView drawerList;
 
